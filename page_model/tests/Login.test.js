4311e0d69c849ac0ca141c6a9ebe724f809b61ab
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2021-03-14 18:07:44 -0600

    test(login): refactor test execution

--- page_model/tests/Login.test.js
+++ page_model/tests/Login.test.js
@@ -1,5 +1,4 @@
 import LoginPage from '../pages/LoginPage'
-import HeaderPage from '../pages/HeaderPage'
 import ProductsPage from '../pages/ProductsPage'
 import MenuPage from '../pages/MenuPage'
 import { CREDENTIALS, SITE }  from '../data/Constants'
@@ -10,27 +9,18 @@ fixture(FEATURE.LOGIN)
 
 test(SCENARIO.L0GIN_VALID_USER, async t => {
     await LoginPage.submitLoginForm(CREDENTIALS.VALID_USER.USERNAME, CREDENTIALS.VALID_USER.PASSWORD)
-    await t.expect(HeaderPage.container.exists).ok()
-    await t.expect(ProductsPage.container.exists).ok()
-    
+    await ProductsPage.verifyLocation()
 })
 
 test(SCENARIO.LOGIN_INVALID, async t => {
     await LoginPage.submitLoginForm(CREDENTIALS.INVALID_USER.USERNAME, CREDENTIALS.INVALID_USER.PASSWORD)
-    await t.expect(LoginPage.errorMessage.exists).ok()
-    await t.expect(LoginPage.errorMessage.innerText).eql(ERROR.INVALID_CREDENTIALS)
+    await LoginPage.verifyErrorMessage(ERROR.INVALID_CREDENTIALS)
 })
 
 test(SCENARIO.LOGOUT_VALID, async t => {
     await LoginPage.submitLoginForm(CREDENTIALS.VALID_USER.USERNAME, CREDENTIALS.VALID_USER.PASSWORD)
-    await t
-        .expect(HeaderPage.container.exists).ok()
-        .expect(ProductsPage.container.exists).ok()
-        .click(HeaderPage.btnBurger)
-        .click(MenuPage.btnLogout)
-        .expect(LoginPage.userNameField.exists).ok()
-        .expect(LoginPage.passwordField.exists).ok()
-        .expect(LoginPage.loginButton.exists).ok()
-
+    await ProductsPage.verifyLocation()
+    await MenuPage.logout();
+    await LoginPage.verifyLocation()
 })
 
