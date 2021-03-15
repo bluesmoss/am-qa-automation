40961be18b8d2b2ec2e0dd719b6e045fdab1624c
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2021-03-14 21:19:35 -0600

    chore(summary): add page and define main elements

---
+++ page_model/pages/SummaryPage.js
@@ -0,0 +1,11 @@
+import { Selector, t } from 'testcafe'
+
+class SummaryPage {
+    constructor(){
+        this.container = Selector('.checkout_summary_container');
+        this.btnFinish = Selector('.btn_action.cart_button').withExactText('FINISH');
+    }
+
+}
+
+export default new SummaryPage()
